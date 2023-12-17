450c9e5fb35685a63ddf37ca4f4716fb74395f8d
yjh <860622588@qq.com>
2023-12-17 13:13:31 +0800

    chore: init repo

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import '@unocss/reset/tailwind.css'
+</script>
+<template>
+  <NuxtLayout >
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style>
+.dark {
+  color-scheme: dark;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const { x, y } = useMouse()
+const color = useColorMode()
+
+function onToggleDark() {
+  color.value = color.value === 'dark' ? 'light' : 'dark'
+}
+</script>
+
+<template>
+  <div text-red>
+    Hello World! {{`(${ x  }, ${y})`}}
+    <button @click="onToggleDark">toggle dark</button>
+  </div>
+</template>
